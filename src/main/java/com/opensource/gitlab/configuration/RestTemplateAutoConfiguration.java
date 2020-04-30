1e538e922aef03380f757ae6e0337207cc5e4fae
sunquan <>
2020-04-30 16:24:11 +0800

    优化RestTemplate构建

--- src/main/java/com/opensource/gitlab/configuration/RestTemplateAutoConfiguration.java
+++ src/main/java/com/opensource/gitlab/configuration/RestTemplateAutoConfiguration.java
@@ -3,10 +3,14 @@ package com.opensource.gitlab.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 /**
  * 初始化RestTemplate配置类
@@ -20,12 +24,14 @@ public class RestTemplateAutoConfiguration {
 
     @ConditionalOnMissingBean(RestTemplate.class)
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate(httpRequestFactory());
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder, HttpComponentsClientHttpRequestFactory httpComponentsClientHttpRequestFactory) {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        restTemplate.setRequestFactory(new BufferingClientHttpRequestFactory(httpComponentsClientHttpRequestFactory));
+        return restTemplate;
     }
 
     @Bean
-    public HttpComponentsClientHttpRequestFactory httpRequestFactory() {
+    public HttpComponentsClientHttpRequestFactory httpComponentsClientHttpRequestFactory() {
         HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory();
         httpRequestFactory.setConnectionRequestTimeout(restProperties.getConnectionRequestTimeout());
         httpRequestFactory.setConnectTimeout(restProperties.getConnectTimeout());
